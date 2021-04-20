ca17884f272cc675c7c9388b5c343972d23fe625
Name-Javi <62718685+Name-Javi@users.noreply.github.com>
2021-04-20 17:34:30 -0500

    troubleshooting

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ const cors = require('cors');
 
 //let allowedOrigins = ['https://javisflix.netlify.app','http://localhost:1234','https://javisolismyflix.herokuapp.com'];
 
-let allowedOrigins = [*]
+app.use(cors());
 
-app.use(cors({
-	origin: (origin, callback) => {
-		if(!origin) return callback(null, true);
-		if(allowedOrigins.indexOf(origin) === -1){
-			let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-			return callback(new Error(message), false);
-		}
-		return callback (null, true);
-	}
-}));
+//app.use(cors({
+	//origin: (origin, callback) => {
+	//	if(!origin) return callback(null, true);
+	//	if(allowedOrigins.indexOf(origin) === -1){
+		//	let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+	//		return callback(new Error(message), false);
+	//	}
+	//	return callback (null, true);
+//}
+//}));
 
 let auth = require('./auth')(app);
 
